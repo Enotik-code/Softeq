c220db3d33272db9db3ee54caba26ae6215edc75
Antony <fvantop@gmail.com>
2020-07-04 16:48:51 +0300

    04.07

---
+++ src/main/java/by/scanner/StringFile.java
@@ -0,0 +1,14 @@
+package by.scanner;
+
+public class StringFile {
+    public static final String ErrorToConnect = "Error to connect to website. Check url or access to site";
+    public static final String ErrorToGetNumberOfWords = "That not a number!";
+
+    public static final String Result = "Result for ";
+    public static final String EnterWebSite = "Please enter website";
+    public static final String EnterCountOfWords = "Please enter a count of words";
+    public static final String EnterYourWord = "Please enter your word";
+
+    public static final String RegexForRemoveTags = "<([\\s\\S]+?)>";
+    public static final String RegexForRemoveJavaScriptCode = "<script>([\\s\\S]+?)<\\/script>";
+}
